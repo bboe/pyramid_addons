e89b6788ef601a70634ad98eddb11b5b754b9162
Bryce Boe <bbzbryce@gmail.com>
2012-10-30 12:55:54 -0700

    Add Simplified BSD License.

--- setup.py
+++ setup.py
@@ -12,22 +12,24 @@ VERSION = re.search("__version__ = '([^']+)'", INIT).group(1)
 
 
 setup(name=PACKAGE_NAME,
-      version=VERSION,
       author='Bryce Boe',
       author_email='bbzbryce@gmail.com',
-      url='https://github.com/bboe/pyramid_addons',
-      description=('A package containing extra needed pyramid functionality '
-                   'including helper functions and form validators.'),
-      long_description=README,
-      keywords='pyramid validation addons',
       classifiers=['Environment :: Console',
                    'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
                    'Natural Language :: English',
                    'Operating System :: OS Independent',
                    'Programming Language :: Python :: 2.6',
                    'Programming Language :: Python :: 2.7',
                    'Programming Language :: Python :: 3.2',
                    'Topic :: Utilities'],
+      description=('A package containing extra needed pyramid functionality '
+                   'including helper functions and form validators.'),
       install_requires=['pyramid>=1.3', 'pytz'],
+      keywords='pyramid validation addons',
+      license='Simplified BSD License',
+      long_description=README,
       packages=[PACKAGE_NAME],
-      test_suite=PACKAGE_NAME)
+      test_suite=PACKAGE_NAME,
+      url='https://github.com/bboe/pyramid_addons',
+      version=VERSION)
